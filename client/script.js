b099d05d05e521a70fee9c98d33c017de4a12968
RMerkulov <fotoromario@gmail.com>
2023-02-04 19:03:56 +0200

    update

--- client/script.js
+++ client/script.js
@@ -79,7 +79,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://codex-im0y.onrender.com/', {
+    const response = await fetch('https://mychat-64tv.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
